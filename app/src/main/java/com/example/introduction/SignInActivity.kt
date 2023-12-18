ca4bcd096960b564d0ef6848ddc21d8a0f3b1127
DESKTOP-8FKP09G\zaq86 <zaq8644@gmail.com>
2023-12-19 01:01:37 +0900

    signin 수정

--- app/src/main/java/com/example/introduction/SignInActivity.kt
+++ app/src/main/java/com/example/introduction/SignInActivity.kt
@@ -13,42 +13,65 @@ import androidx.appcompat.app.AppCompatActivity
 
 class SignInActivity : AppCompatActivity() {
 
+    private val signInButton: Button by lazy {
+        findViewById(R.id.btn_sign_in)
+    }
+
+    private val signUpButton: Button by lazy {
+        findViewById(R.id.btn_create_account)
+    }
 
     private lateinit var idChk: EditText
     private lateinit var pasChk: EditText
-    private val getContent = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-        if (result.resultCode == Activity.RESULT_OK) {
-            val id = result.data?.getStringExtra("id")
-            val password = result.data?.getStringExtra("password")
+    private val getContent =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val id = result.data?.getStringExtra("id")
+                val password = result.data?.getStringExtra("password")
 
-            idChk.setText(id)
-            pasChk.setText(password)
+                idChk.setText(id)
+                pasChk.setText(password)
+            }
         }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
 
+        init()
+    }
+
+    private fun init() {
+        setSigUpUserData()
+
+        setSignInButton()
+
+        setSignUpButton()
+    }
+
+    private fun setSigUpUserData() {
         idChk = findViewById(R.id.input_id)
         pasChk = findViewById(R.id.input_pass)
-        val signInButton = findViewById<Button>(R.id.btn_sign_in)
-        val signUpButton = findViewById<Button>(R.id.btn_create_account)
-        //테스트용 아이디 입니다
-        UserList.userList.add(User("a","a","a","a","",0,"ㅎㅇ","1980-01-01"))
+    }
 
+    private fun setSignInButton() {
         signInButton.setOnClickListener {
             val intent = Intent(this, HomeActivity::class.java)
 
-
-            if (UserList.userList.none { it.id == idChk.text.toString() }) Toast.makeText(this,"가입되지 않은 사용자입니다.",Toast.LENGTH_SHORT).show()
-            else if (UserList.userList.any { it.id == idChk.text.toString() && it.password == pasChk.text.toString()  }) {
+            if (UserList.userList.none { it.id == idChk.text.toString() }
+            ) Toast.makeText(this, getString(R.string.no_one_user_list), Toast.LENGTH_SHORT).show()
+            else if (UserList.userList.any { it.id == idChk.text.toString() && it.password == pasChk.text.toString() }) {
                 intent.putExtra("id", idChk.text.toString())
                 startActivity(intent)
-            }
-            else Toast.makeText(this, "아이디 또는 비밀번호가 잘못되었습니다",Toast.LENGTH_SHORT).show()
+            } else Toast.makeText(
+                this,
+                getString(R.string.check_id_password_plz),
+                Toast.LENGTH_SHORT
+            ).show()
         }
+    }
 
+    private fun setSignUpButton() {
         signUpButton.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
             getContent.launch(intent)
