911399fc4327385eee47ed76cbd7be9a33215bf0
DESKTOP-8FKP09G\zaq86 <zaq8644@gmail.com>
2024-01-10 02:18:58 +0900

    refactor: "🔨 repository 파사드(Parcade패턴으로 구성)"
    인터페이스에 함수만 정렬해서 사용자가 복잡한 내부구조를 몰라도 사용할 수 있게 함

--- app/src/main/java/com/example/introduction/UserRepository.kt
+++ app/src/main/java/com/example/introduction/UserRepository.kt
@@ -1,16 +1,31 @@
 package com.example.introduction
 
-class UserRepository {
+/**
+ * TODO 파사드 패턴으로 Repository 구성
+ *
+ * 사용자는 함수의 복잡한 로직을 알 필요 없이 함수만 보고 사용가능하다
+ */
+interface UserData {
+    // 모든 유저 데이터 갱신
+    fun loadUser(userData: ArrayList<User>)
+    // 유저데이터에서 입력받은 id로 유저를 찾아냄
+    fun findUserById(id: String): User?
+    // 기존 유저의 정보를 업데이트함
+    fun updateUser(user: User)
+    // 새로운 유저의 정보를 저장함
+    fun saveUser(newUser: User)
+}
+class UserRepository: UserData {
     var userList = ArrayList<User>()
 
-    fun loadUser(userData: ArrayList<User>) {
+    override fun loadUser(userData: ArrayList<User>) {
         userList = userData
     }
 
-    fun findUserById(id: String): User? {
+    override fun findUserById(id: String): User? {
         return userList.find { it.id == id }
     }
-    fun updateUser(user: User) {
+    override fun updateUser(user: User) {
         userList.find { it.id == user.id }?.let {
             it.name = user.name
             it.password = user.password
@@ -18,7 +33,7 @@ class UserRepository {
         }
     }
 
-    fun saveUser(newUser: User) {
+    override fun saveUser(newUser: User) {
         userList.add(newUser)
     }
 }
--- app/src/main/java/com/example/introduction/signup/SignUpUseCase.kt
+++ app/src/main/java/com/example/introduction/signup/SignUpUseCase.kt
@@ -7,6 +7,11 @@ import com.example.introduction.signup.SignUpValidExtension.includeSpecialCharac
 import com.example.introduction.signup.SignUpValidExtension.includeUpperCase
 import com.example.introduction.signup.SignUpValidExtension.validEmailServiceProvider
 
+/**
+ * TODO 회원가입&수정 페이지에서 유효성검사
+ *
+ * EditText의 TYPE을 받고 그에 맞게 유효성을 검사하고 메세지를 반환함
+ */
 class UseCaseErrorMessage {
     operator fun invoke(type: EditType, text: String): SignUpErrorMessage? {
         val error: SignUpErrorMessage? = when {
@@ -46,6 +51,12 @@ class UseCaseErrorMessage {
     }
 }
 
+/**
+ * TODO 이메일 스피너 위치 반환
+ *
+ * 사용자가 회원가입이아닌 수정으로 Entry했을때 정보를 바탕으로 사용자의 email service가 이메일 스피너에 있는 service인지 확인하고
+ * 만약 스피너에 없으면 직접 입력을 있으면 그위치에 맞는 스피너를 세팅하게 한다
+ */
 class UseCaseServiceIndex{
     operator fun invoke(service: String?, emails: Array<String>): Int {
         return if (emails.any { it == service }) {
@@ -54,6 +65,9 @@ class UseCaseServiceIndex{
     }
 }
 
+/**
+ * TODO 비밀번호 확인
+ */
 class CheckPassword {
     operator fun invoke(password: String, confirmPassword: String): SignUpErrorMessage? {
         return if (password == confirmPassword) null
@@ -61,6 +75,9 @@ class CheckPassword {
     }
 }
 
+/**
+ * TODO 신규 사용자 데이터 레포지토리에 저장
+ */
 class SaveUser(private val userRepository: UserRepository) {
     operator fun invoke(name: String, id: String, emailId: String, emailService: String, password: String, idVisible: Boolean) {
         val email = "$emailId@$emailService"
